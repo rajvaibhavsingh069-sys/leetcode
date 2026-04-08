de0f4347a3afc8747382a916a1d9dbbe7bbf8231
rajvaibhavsingh069-sys <rajvaibhavsingh069@gmail.com>
2026-04-08 23:20:06 +0530

    Time: 20 ms (85.72%) | Memory: 20 MB (24.33%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -1,26 +1,41 @@
 class Solution:
-    def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        
+    def findSubstring(self, s: str, words: list[str]) -> list[int]:
+        from collections import Counter, defaultdict
+
         if not s or not words:
             return []
 
         word_len = len(words[0])
-        total_words = len(words)
-        total_len = word_len * total_words
+        n = len(words)
+        total_len = word_len * n
+        freq = Counter(words)
 
         res = []
 
-        for i in range(len(s) - total_len + 1):
-            seen = []
-            
-            for j in range(total_words):
-                start = i + j * word_len
-                word = s[start:start + word_len]
-                seen.append(word)
+        for i in range(word_len):
+            left = i
+            curr = defaultdict(int)
+            count = 0
 
-            
-            if sorted(seen) == sorted(words):
-                res.append(i)
+            for right in range(i, len(s) - word_len + 1, word_len):
+                word = s[right:right + word_len]
+
+                if word in freq:
+                    curr[word] += 1
+                    count += 1
+
+                    while curr[word] > freq[word]:
+                        left_word = s[left:left + word_len]
+                        curr[left_word] -= 1
+                        left += word_len
+                        count -= 1
+
+                    if count == n:
+                        res.append(left)
+
+                else:
+                    curr.clear()
+                    count = 0
+                    left = right + word_len
 
         return res
-        
